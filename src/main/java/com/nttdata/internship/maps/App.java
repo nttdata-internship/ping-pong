d8cbce0a28b151b00574f4af767a1ef97ccf55bf
Alexandru Ciocan <alexandru.ciocan@ebsromania.com>
2017-07-12 16:34:23 +0300

    (feature)(maps): Solution for finding the min/max temperature in each country

--- src/main/java/com/nttdata/internship/maps/App.java
+++ src/main/java/com/nttdata/internship/maps/App.java
@@ -1,9 +1,20 @@
 package com.nttdata.internship.maps;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import com.nttdata.internship.maps.databind.ObjectReader;
+import com.nttdata.internship.maps.entity.Country;
 import com.nttdata.internship.maps.entity.Location;
 
 //HashMap cu locatii ca si key si valoarea va fi temperatura maxima a locatiei.
@@ -16,15 +27,74 @@ import com.nttdata.internship.maps.entity.Location;
  */
 public class App {
 
+	private static final String MIN_TEMPERATURE = "MIN";
+	private static final String MAX_TEMPERATURE = "MAX";
+
 	public static void main(String[] args) {
 
 		ObjectReader<Location> objectReader = new ObjectReader<Location>("locations.json", Location.class);
 		try {
-			List<Location> location = (List<Location>) objectReader.readList();
+			List<Location> locations = objectReader.readList();
 
-			System.out.println(location.size());
-			
-			location.forEach(l -> System.out.println(l.getCity()));
+			System.out.println(locations.size());
+
+			Map<Location, Float> weatherData = new TreeMap<Location, Float>(new Comparator<Location>() {
+
+				public int compare(Location o1, Location o2) {
+					if (o1.getCountry().equals(o2.getCountry())) {
+						return o1.getCity().compareTo(o2.getCity());
+					} else
+						return o1.getCountry().compareTo(o2.getCountry());
+				}
+			});
+
+			for (Location location : locations) {
+				weatherData.put(location, location.getTemperature());
+
+			}
+
+			// TODO citeste de la consola si apeleaza getter
+
+			Iterator<Location> locationIt = weatherData.keySet().iterator();
+			Country currentCountry = null;
+			float minTemperature = 0;
+			float maxTemperature = 0;
+			Map<Country, Map<String, Location>> weatherStatistics = new HashMap<Country, Map<String, Location>>();
+			while (locationIt.hasNext()) {
+				Location currentLocation = locationIt.next();
+				float currentTemperature = currentLocation.getTemperature();
+				Map<String, Location> countryStatistics = weatherStatistics.get(currentLocation.getCountry());
+				if (currentCountry != currentLocation.getCountry()) {
+					countryStatistics = new HashMap<String, Location>();
+					countryStatistics.put(MAX_TEMPERATURE, currentLocation);
+					countryStatistics.put(MIN_TEMPERATURE, currentLocation);
+					weatherStatistics.put(currentLocation.getCountry(), countryStatistics);
+					minTemperature = currentTemperature;
+					maxTemperature = currentTemperature;
+
+				}
+				if (currentTemperature < minTemperature) {
+					countryStatistics.put(MIN_TEMPERATURE, currentLocation);
+					minTemperature = currentTemperature;
+				}
+				if (currentTemperature > maxTemperature) {
+					countryStatistics.put(MAX_TEMPERATURE, currentLocation);
+					maxTemperature = currentTemperature;
+				}
+				currentCountry = currentLocation.getCountry();
+
+			}
+
+			for (Entry<Country, Map<String, Location>> countryData : weatherStatistics.entrySet()) {
+				System.out.println("Country Statistic for country=[" + countryData.getKey() + "]");
+				Map<String, Location> cityData = countryData.getValue();
+				System.out.println(String.format("Min Temparature %s for city %s",
+						cityData.get(MIN_TEMPERATURE).getTemperature(), cityData.get(MIN_TEMPERATURE).getCity()));
+
+				System.out.println(String.format("Max Temparature %s for city %s",
+						cityData.get(MAX_TEMPERATURE).getTemperature(), cityData.get(MAX_TEMPERATURE).getCity()));
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/nttdata/internship/maps/entity/Location.java
+++ src/main/java/com/nttdata/internship/maps/entity/Location.java
@@ -15,9 +15,16 @@ public class Location implements Serializable {
 
 	private String region;
 
+	public Location() {
+	}
+
+	public Location(String region) {
+		super();
+		this.region = region;
+	}
+
 	private float temperature;
 
-	
 	public float getTemperature() {
 		return temperature;
 	}
@@ -49,5 +56,41 @@ public class Location implements Serializable {
 	public void setRegion(String region) {
 		this.region = region;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((region == null) ? 0 : region.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country != other.country)
+			return false;
+		if (region == null) {
+			if (other.region != null)
+				return false;
+		} else if (!region.equals(other.region))
+			return false;
+		return true;
+	}
+
+
 
 }
