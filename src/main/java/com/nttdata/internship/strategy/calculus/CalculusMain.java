0a3fed7acea398e97e21ad8da1d4339b9f9ae194
ioanaaaaa <ioana.alexa_96@yahoo.com>
2017-07-17 12:35:50 +0300

    Perimeter mock

---
+++ src/main/java/com/nttdata/internship/strategy/calculus/CalculusMain.java
@@ -0,0 +1,20 @@
+package com.nttdata.internship.strategy.calculus;
+
+public class CalculusMain {
+
+	// O sa avem o interfata pe nume Perimeter;
+	//Clase cerg, padrad , dreptunghi ,triunghi 
+	public static void main(String[] args) {
+		Perimeter p=null;
+		calculate(p);
+		
+	}
+
+	private static int calculate(Perimeter p) {
+		// TODO Auto-generated method stub
+		return -1;
+	}
+	
+	
+	
+}
