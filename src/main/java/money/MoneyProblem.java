8a1aa5c33d89af8673766548ff08d5bf40592a4b
ioanaaaaa <ioana.alexa_96@yahoo.com>
2017-07-31 16:45:00 +0300

    BigDecimal reloaded

--- src/main/java/money/MoneyProblem.java
+++ src/main/java/money/MoneyProblem.java
@@ -1,14 +1,17 @@
 package money;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Currency;
+
+import javax.swing.plaf.RootPaneUI;
 
 public class MoneyProblem {
     public static void main(String... aArgs) {
         //TODO: transform first 2 arguments in big decimal values
     	
-    	BigDecimal two = new BigDecimal(new String ("100"));
-    	BigDecimal one = new BigDecimal(new String ("200"));
+    	BigDecimal two = new BigDecimal(new String ("200"));
+    	BigDecimal one = new BigDecimal(new String ("100"));
         MoneyProblem calc = new MoneyProblem(one, two);
         calc.doCalculations();
     }
@@ -26,6 +29,10 @@ public class MoneyProblem {
         log("Average : " + getAverage());
         log("5.25% of Amount One: " + getPercentage());
         log("Percent Change From Amount One to Two: " + getPercentageChange());
+        log("Decimal x divided to decimal y : " + divideTwoNumbers());
+        log("int c divided to int y : " + divideTwoNumbersInteger());
+        log("compareto : " + compareTwoNumbers());
+        log("equals : " + equals());
     }
 
     private BigDecimal fAmountOne;
@@ -47,7 +54,8 @@ public class MoneyProblem {
     private static final BigDecimal TWO = new BigDecimal("2");
     private static BigDecimal HUNDRED = new BigDecimal("100");
     private static BigDecimal PERCENTAGE = new BigDecimal("5.25");
-
+    private static BigDecimal PERCENTAGE2 = new BigDecimal("17.9");
+    
     private void log(String aText) {
         System.out.println(aText);
     }
@@ -59,7 +67,7 @@ public class MoneyProblem {
     
 
     private BigDecimal getDifference() {
-        return this.fAmountOne.subtract(fAmountTwo);
+        return this.fAmountOne.subtract(fAmountTwo).abs();
     }
 
     private BigDecimal getAverage() {
@@ -71,7 +79,7 @@ public class MoneyProblem {
      * @return 5.25% from first value plus 17.9% from second value
      */
     private BigDecimal getPercentage() {
-        return PERCENTAGE.add(fAmountOne);
+        return PERCENTAGE.multiply(fAmountOne).divide(HUNDRED).add(PERCENTAGE2.multiply(fAmountTwo).divide(HUNDRED));
     }
 
     /**
@@ -79,11 +87,38 @@ public class MoneyProblem {
      * @return PercentageChange from value 1 to value 2
      */
     private BigDecimal getPercentageChange() {
-        return fAmountTwo.add(getPercentage());
+        return fAmountTwo.multiply(HUNDRED).divide(fAmountOne);
         //TODO
     }
 
     private BigDecimal rounded(BigDecimal aNumber) {
-        return aNumber.setScale(DECIMALS, ROUNDING_MODE);
+        return aNumber.setScale(7,7);
     }
+    
+    private BigDecimal divideTwoNumbers() {
+    	BigDecimal a = rounded(new BigDecimal("10"));
+    	BigDecimal b = rounded(new BigDecimal("3"));
+        return a.divide(b,RoundingMode.HALF_DOWN);
+    }
+    
+    private float divideTwoNumbersInteger() {
+    	float a = 10;
+    	float b = 3;
+        return  a/b;
+    }
+    
+    private int compareTwoNumbers(){
+    	BigDecimal b = divideTwoNumbers();
+    	BigDecimal a = BigDecimal.valueOf(divideTwoNumbersInteger());
+    	return b.compareTo(a);
+    }
+    
+    private boolean equals(){
+    	BigDecimal b = divideTwoNumbers();
+    	BigDecimal a = BigDecimal.valueOf(divideTwoNumbersInteger());
+    	return b.equals(a);
+    	
+    	
+    }
+    
 }
